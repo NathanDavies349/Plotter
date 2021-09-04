6f723e2e27dcd888828ad09eb3c6afba4360acc8
Nathan <Cooper2311@hotmail.co.uk>
2021-09-04 20:55:26 +0100

    Comments added

--- Plot.py
+++ Plot.py
@@ -5,7 +5,6 @@ style.use('seaborn-dark')
 #----
 #Font size adjustment
 #Validate parameters
-#
 #----
 
 class Plot():
@@ -24,14 +23,14 @@ class UnivariatePlot(Plot):
   
   #def Histogram(self):
 
-class MultivariatePlot(Plot):
+class MultivariatePlot(Plot):#Class that handles any plot where two parameters are needed, i.e. an x and a y variable
   def __init__(self, x_label, y_label, title, residual=False):
     super().__init__(x_label, y_label, title)
 
-    if residual == False:
+    if residual == False:#this produces only one set of axes
       self._fig, (self._ax1) = plt.subplots(nrows=1, ncols=1, figsize=(8,8))
       self._ax1.set_xlabel(self._x_label)
-    elif residual == True:
+    elif residual == True:#this produces two axes, with an adjusted height ratio. The bottom set of axes is the residual.
       self._fig, (self._ax1, self._ax2) = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(8,8), gridspec_kw={'height_ratios': [4, 1], 'top': 0.95, 'hspace': 0.1})
       self._ax2.grid(True)
       self._ax2.set_ylabel('Residual')
